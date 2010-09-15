08ec243fcb2eb225dd38e6b20054238d497d5e4c
ashishtomar123 <ashishtomar123@9d938fcf-59aa-a747-8ab3-0ceac2fe3b86>
2010-09-15 19:33:21 +0000

    NLP assignment 1 code structure

---
+++ trunk/src/FileParser.java
@@ -0,0 +1,172 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class FileParser {
+
+	public static Hashtable Verbs = new Hashtable();
+	public static Hashtable Nouns = new Hashtable();
+
+	public static void main(String args[]){
+		int cnt =0;
+
+		if(args.length==2)
+		{
+
+
+			try
+			{
+
+
+
+
+
+
+				FileInputStream fstream2 = new FileInputStream("question.txt");
+				DataInputStream in2 = new DataInputStream(fstream2);
+				BufferedReader br2 = new BufferedReader(new InputStreamReader(in2));
+				String strLine2;
+				while ((strLine2 = br2.readLine()) != null)
+				{
+					//cnt++;
+					System.out.println(strLine2);
+					parseQuestion(strLine2);
+					//if(cnt==4)
+					//	break;
+				}
+
+				in2.close();
+
+
+
+
+
+				FileInputStream fstream = new FileInputStream("posfile.txt");
+				DataInputStream in = new DataInputStream(fstream);
+				BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				String strLine;
+				while ((strLine = br.readLine()) != null)
+				{
+					cnt++;
+					//System.out.println(strLine);
+					parseLine(strLine);
+					//if(cnt==4)
+					//	break;
+				}
+
+				in.close();
+
+
+
+				System.out.println();
+				System.out.println("Verbs "+Verbs.size());
+				printTable(Verbs);
+				System.out.println();
+				System.out.println("Nouns "+Nouns.size());
+				printTable(Nouns);
+
+
+			}
+			catch (Exception e)
+			{
+				System.err.println("Error: " + e.getMessage());
+				e.printStackTrace();
+			}
+
+
+
+
+		}
+		else
+		{
+			System.out.println("Usage--");
+			System.out.println("First parameter -- Input file");
+			System.out.println("Second parameter -- Question file");
+
+		}
+	}
+
+
+
+
+	private static void printTable(Hashtable ht) {
+
+
+		Enumeration e = ht.keys();
+		while( e. hasMoreElements() ){
+			System.out.println( e.nextElement() );
+		}
+
+	}
+
+
+	private static void parseLine(String strLine) {
+		try {
+			// Create matcher on file
+			String st[] =strLine.split(" ");
+			for(int i=0;i<st.length;i++){
+
+				getVerbs(st[i]);
+
+				getNouns(st[i]);
+
+			}
+		} catch (Exception e) {
+		}
+
+	}
+
+
+	private static void getVerbs(String str) {
+		Pattern pattern = Pattern.compile("(.*)/VB");
+		Matcher matcher = pattern.matcher(str);
+
+		// Find all matches
+		while (matcher.find()) {
+			// Get the matching string
+			String match = matcher.group(1);
+			if(!Verbs.containsKey(match))
+				Verbs.put(match, "1");
+			//System.out.println(match);
+		}
+	}
+
+
+
+	private static void getNouns(String str) {
+		Pattern pattern = Pattern.compile("(.*)/NN");
+		Matcher matcher = pattern.matcher(str);
+
+		// Find all matches
+		while (matcher.find()) {
+			// Get the matching string
+			String match = matcher.group(1);
+			if(!Nouns.containsKey(match))
+				Nouns.put(match, "1");
+			//System.out.println(match);
+		}
+	}
+
+
+
+
+	private static void parseQuestion(String strLine2) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+
+
+
+}
