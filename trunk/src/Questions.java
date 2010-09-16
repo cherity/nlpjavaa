5cff5569e2e3ad1bd319beb78fd88917e0a1b481
ashishtomar123 <ashishtomar123@9d938fcf-59aa-a747-8ab3-0ceac2fe3b86>
2010-09-16 02:35:06 +0000

    all questions parsed need 8 answers

--- trunk/src/Questions.java
+++ trunk/src/Questions.java
@@ -10,8 +10,8 @@ public class Questions {
 		questionList.add(".*[Ww]hat did .*?([A-Z].*) do\\?");
 
 
-		questionList.add(".*[Hh]ow much *did .*?([A-Z].*).* (fall|fell|loose|lost|collapse|collapsed|crash|crashed|dip|decline|declined|down|sink)\\?");
-		questionList.add(".*[Hh]ow much *did .*?([A-Z].*).* (rise|gain|gained|rose|grow|raised|raise|hike|up)\\?");
+		questionList.add(".*[Hh]ow much did .*?([A-Z].*).* (fall|fell|loose|lost|collapse|collapsed|crash|crashed|dip|decline|declined|down|sink)\\?");
+		questionList.add(".*[Hh]ow much did .*?([A-Z].*).* (rise|gain|gained|rose|grow|raised|raise|hike|up)\\?");
 
 
 		questionList.add(".*[Dd]id .*?([A-Z].*).*? (rise|fall|gain|gained|fell|rose|loose|lost|grow|raised|raise|hike|up|collapse|collapsed|crash|crashed|dip|decline|declined|down|sink) or .*?(rise|fall|gain|gained|fell|rose|loose|lost|grow|raised|raise|hike|up|collapse|collapsed|crash|crashed|dip|decline|declined|down|sink)\\?");
@@ -19,15 +19,15 @@ public class Questions {
 		questionList.add(".*[Ww]hat did .*?([A-Z].*) close at\\?");
 
 
-		questionList.add(".*[Ww]hat.*?(index|indexes).*(fall|fell|loose|lost|collapse|collapsed|crash|crashed|dip|decline|declined|down|sink)\\?");
-		questionList.add(".*[Ww]hat.*?(index|indexes).*(rise|gain|gained|rose|grow|raised|raise|hike|up)\\?");
+		questionList.add(".*[Ww](hat|hich).*?(index|indexes).*(fall|fell|loose|lost|collapse|collapsed|crash|crashed|dip|decline|declined|down|sink)\\?");
+		questionList.add(".*[Ww](hat|hich).*?(index|indexes).*(rise|gain|gained|rose|grow|raised|raise|hike|up)\\?");
 
-		questionList.add(".*[Ww]hat was (the)? (.*)\\?");
+		questionList.add(".*[Ww]hat was\\s?(the)?\\s?(.*)\\?");
 
-		questionList.add(".*[Ww]hat did\\s?(the)?\\s?(.*) do against (the)? (.*)\\?");
+		questionList.add(".*[Ww]hat did\\s?(the)?\\s?(.*) do against\\s?(the)?\\s?(.*)\\?");
 
-		questionList.add(".*[Ww](hat|hich)\\s?(company)?\\s?(stock|stocks) .*(fall|fell|loose|lost|collapse|collapsed|crash|crashed|dip|decline|declined|down|sink)\\?");
-		questionList.add(".*[Ww](hat|hich)\\s?(company)?\\s?(stock|stocks) .*(rise|gain|gained|rose|grow|raised|raise|hike|up)\\?");
+		questionList.add(".*[Ww](hat|hich)\\s?(company)?\\s?(stock|stocks)\\s?.*(fall|fell|loose|lost|collapse|collapsed|crash|crashed|dip|decline|declined|down|sink)\\?");
+		questionList.add(".*[Ww](hat|hich)\\s?(company)?\\s?(stock|stocks)\\s?.*(rise|gain|gained|rose|grow|raised|raise|hike|up)\\?");
 
 
 	}
