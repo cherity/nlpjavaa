dbedb99595ae11d7bf311a1f8757f63fbf6e8aa0
ashishtomar123 <ashishtomar123@9d938fcf-59aa-a747-8ab3-0ceac2fe3b86>
2010-09-30 21:25:56 +0000

    Commented FileParser.java

--- trunk/src/Answer.java
+++ trunk/src/Answer.java
@@ -3,16 +3,10 @@
  * ast2124
  * NLP Assignment # 1
  * Stock market QA System
+ * Answer.java
  */
 
 
-
-
-
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -1366,23 +1360,7 @@ public class Answer {
 		}
 	}
 
-	private String getDiffNoun(String nounCase) {
-
-		String strnoun[] =nounCase.split(" ");
-		String newnoun="";
-		int i=0;
-		for(String str:strnoun){
-			i++;
-			if(i==strnoun.length){
-				newnoun+=str;
-			}else{
-				newnoun+=str+"\\s?.*?\\s?";
-			}
-
-		}
-
-		return newnoun;
-	}
+	
 
 	private String modifyString(String strLine) {
 
