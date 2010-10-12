aafe73baeee09cf1aefac67781303da78b330c83
ashishtomar123 <ashishtomar123@9d938fcf-59aa-a747-8ab3-0ceac2fe3b86>
2010-10-12 22:37:24 +0000

    pos neg arraylist added

--- trunk/assn2/src/code/Document.java
+++ trunk/assn2/src/code/Document.java
@@ -5,12 +5,16 @@ import java.util.Hashtable;
 public class Document {
 
 	public double countWords; 
+	public double posWords; 
+	public double negWords; 
 	public int star; 
 	public  Hashtable<String, Integer> termFrequency;
 
 	public Document(){
 		star=1;
 		countWords=0;
+		posWords=0;
+		negWords=0;
 		termFrequency= new Hashtable<String, Integer>();
 
 	}
