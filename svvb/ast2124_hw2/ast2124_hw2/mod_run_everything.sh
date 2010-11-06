262a3de3ac8cdb648a33e3b974e82a4b377ac665
ashishtomar123 <ashishtomar123@9d938fcf-59aa-a747-8ab3-0ceac2fe3b86>
2010-11-06 08:44:46 +0000

    files updated

--- svvb/ast2124_hw2/ast2124_hw2/mod_run_everything.sh
+++ svvb/ast2124_hw2/ast2124_hw2/mod_run_everything.sh
@@ -1,6 +1,6 @@
 #!/bin/bash   
-# 3 args
-#train.txt test.txt op 
+# 2 args
+#train.txt test.txt  
 
 #train
 
@@ -97,10 +97,18 @@ java -cp bin/.:/home/cs4705/stanford-parser-2010-08-20/stanford-parser-2010-08-2
 
 
 
-./run_weka_on_test.sh starRatingSameUsers.model starRatingSameUsersTest.arff out_starRatingSameUsersTest.arff multi "$2" "$3"
-./run_weka_on_test.sh starRatingDiffUsers.model starRatingDiffUsersTest.arff out_starRatingDiffUsersTest.arff multi "$2" "$3"
-./run_weka_on_test.sh binaryRatingSameUsers.model binaryRatingSameUsersTest.arff out_binaryRatingSameUsersTest.arff binary "$2" "$3"
-./run_weka_on_test.sh binaryRatingDiffUsers.model binaryRatingDiffUsersTest.arff out_binaryRatingDiffUsersTest.arff binary "$2" "$3"
-./run_weka_on_test.sh author.model authorTest.arff out_authorTest.arff reviewer "$2" "$3"
+#./run_weka_on_test.sh starRatingSameUsers.model starRatingSameUsersTest.arff out_starRatingSameUsersTest.arff multi "$2" "$3"
+#./run_weka_on_test.sh starRatingDiffUsers.model starRatingDiffUsersTest.arff out_starRatingDiffUsersTest.arff multi "$2" "$3"
+#./run_weka_on_test.sh binaryRatingSameUsers.model binaryRatingSameUsersTest.arff out_binaryRatingSameUsersTest.arff binary "$2" "$3"
+#./run_weka_on_test.sh binaryRatingDiffUsers.model binaryRatingDiffUsersTest.arff out_binaryRatingDiffUsersTest.arff binary "$2" "$3"
+#./run_weka_on_test.sh author.model authorTest.arff out_authorTest.arff reviewer "$2" "$3"
+
+
+./run_weka_on_test.sh starRatingSameUsers.model starRatingSameUsersTest.arff out_starRatingSameUsersTest.arff multi "$2" classified-starRatingSameUsersTest.txt
+./run_weka_on_test.sh starRatingDiffUsers.model starRatingDiffUsersTest.arff out_starRatingDiffUsersTest.arff multi "$2" classified-starRatingDiffUsersTest.txt
+./run_weka_on_test.sh binaryRatingSameUsers.model binaryRatingSameUsersTest.arff out_binaryRatingSameUsersTest.arff binary "$2" classified-binaryRatingSameUsersTest.txt
+./run_weka_on_test.sh binaryRatingDiffUsers.model binaryRatingDiffUsersTest.arff out_binaryRatingDiffUsersTest.arff binary "$2" classified-binaryRatingDiffUsersTest.txt
+./run_weka_on_test.sh author.model authorTest.arff out_authorTest.arff reviewer "$2" classified-authorTest.txt
+
 
 
